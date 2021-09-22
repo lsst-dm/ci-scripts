c7bd846201bd13d80f749f7f419007afb19b7bcc
Simon Krughoff <simon.krughoff@gmail.com>
2021-09-22 09:32:26 -0700

    Put building jointcal in a subshell

--- run_verify_drp_metrics.sh
+++ run_verify_drp_metrics.sh
@@ -97,11 +97,13 @@ setup -k -r "$LSST_VERIFY_DRP_METRICS_DATASET_DIR"
 set -o xtrace
 
 ############ This is a hack to get around the fact that jointcal uses configs that are stripped in the container
+
 git clone https://github.com/lsst/jointcal
+setup -k -r jointcal
+(
 cd jointcal
-setup -k -r .
 scons
-cd ..
+)
 ########### 
 
 case "$LSST_VERIFY_DRP_METRICS_DATASET" in
