562009a296fe42e0a132c73329be3789f89c1f43
Ross Ceballo <ross.ceballo@noirlab.edu>
2025-07-30 15:31:36 -0400

    Add script to store and load lsstsw

---
+++ backuplsststack.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -xeo pipefail
+
+# This script creates a tarball of the lsstsw directory and copies it to GCP_BUCKET for later usage
+# and is only intended to be useful when executed by jenkins.  It assumes that the `lsst/lsstsw`
+# repo have already been cloned into the jenkins
+# `$WORKSPACE`.
+
+ROOT_DIR="$(dirname "$(pwd)")"
+LSSTSW_DIR="${ROOT_DIR}/lsstsw"
+
+GCP_BUCKET="gs://eups-lsstsw-cache"
+TMP_FOLDER="/tmp/lsstswcache"
+
+fail() {
+  echo "$1" >&2
+  exit 1
+}
+
+[[ $# -eq 0 ]] && fail "No tag was passed" 
+DATE_TAG=$1
+
+mkdir -p "$TMP_FOLDER"
+ARCH_VER=$(uname -m)
+
+TARGET="${TMP_FOLDER}/${DATE_TAG}_${ARCH_VER}_lsstsw.tar.gz" 
+
+cd "$ROOT_DIR" || fail "Can't find ${ROOT_DIR}"
+
+[[ -d "$LSSTSW_DIR" ]] || fail "Can't find lsstsw dir"
+[[ -d "$LSSTSW_DIR/miniconda" ]] || fail "Can't find miniconda dir"
+[[ -d "$LSSTSW_DIR/stack" ]] || fail "Can't find stack dir"
+[[ -d "$LSSTSW_DIR/build" ]] || fail "Can't find build dir"
+
+tar -cvzf "${TARGET}" lsstsw
+
+gcloud storage cp "${TARGET}" "${GCP_BUCKET}"
+
+rm "${TARGET}"
